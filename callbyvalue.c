df1110d699753c49a420087a8a7411b8e0b1124f
Alex01921 <learnhtml1921@gmail.com>
2025-04-03 01:42:46 +0530

    Create callbyvalue.c

---
+++ callbyvalue.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int swap(int x,int y);
+int main()
+{
+    int a,b;
+    printf("Enter the values for a and b:");
+    scanf("%d%d",&a,&b);
+    printf("\nThe values of a and b before fucntion call are %d and %d",a,b);
+    swap(a,b);
+    printf("\nThe values of a and b after fucntion call are %d and %d",a,b);
+    return 0;
+}
+int swap(int x, int y)
+{
+    int t;
+    t = x;
+    x = y;
+    y = t;
+    printf("\nThe values of a and b inside fucntion  are %d and %d",x,y);
+}
